4c5dc02ec4dbe69eef09e479bcbc86bc2fafd9da
DotZZ <93223349+DaRealDotZ@users.noreply.github.com>
2023-01-17 06:33:40 -0500

    Here we go again: 3

--- source/engine/optionsMenu/TextOption.hx
+++ source/engine/optionsMenu/TextOption.hx
@@ -8,15 +8,29 @@ class TextOption extends FlxText
 {
     public var funnyOptionType:Int = 0; // 0 - on/off | 1 - New Menu | 2 - Switch State
 
-    public function new(x:Float, y:Float, text:String, optionType:Int){
+    public function new(x:Float, y:Float, optText:String, optionType:Int, ?option:Bool = null){
         super(x, y, FlxG.width, text, 72);
+
+        if (option != null)
+        {
+            text = optText + ' ${option  ? 'ON' : 'OFF'}';
+        }
+        else
+        {
+            text = optText;
+        }
 
         setFormat("PhantomMuff 1.5", 72, FlxColor.WHITE, "center");
         setBorderStyle(FlxTextBorderStyle.OUTLINE, 0xFF000000, 5, 1);
         setSize(width, height * 1.25);
         antialiasing = true;
 
-        funnyOptionType = optionType;
+        this.funnyOptionType = optionType;
+    }
+
+    public function refreshText(optText:String,option:Bool)
+    {
+        text = optText + ' ${option  ? 'ON' : 'OFF'}';
     }
 
     override public function update(elapsed:Float)
