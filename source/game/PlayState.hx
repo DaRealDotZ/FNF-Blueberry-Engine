f098d6dde419da659545fe8977a4f4cdcb5902bd
DotZZ <93223349+DaRealDotZ@users.noreply.github.com>
2023-01-17 06:36:30 -0500

    Oops

--- source/game/PlayState.hx
+++ source/game/PlayState.hx
@@ -167,7 +167,7 @@ class PlayState extends MusicBeatState
 		FlxG.cameras.reset(camGame);
 		FlxG.cameras.add(camHUD);
 
-		FlxG.cameras.setDefaultDrawTarget(camGame,true);
+		FlxCamera.defaultCameras = [camGame];
 
 		persistentUpdate = true;
 		persistentDraw = true;
